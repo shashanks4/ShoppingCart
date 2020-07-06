1e01263d0f354be239aaf1410024cad703a9626a
shashanks4 <49721667+shashanks4@users.noreply.github.com>
2020-07-06 14:38:20 +0530

    Add files via upload

---
+++ Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCart.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+       
+        public string Customer { get; set; }
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+    }
+}
---
+++ Models/OrderDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCart.Models
+{
+
+    public class OrderDetails
+    {
+        public class Detail
+        {
+            public int ProductID { get; set; }
+            public string Product { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+        }
+        public List<Detail> Details { get; set; }
+    }
+}
